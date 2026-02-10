244ba786f0e01aacdb438b0172c68eddff0ac52e
Bhawna Kushwaha <bhawnakushwaha727@gmail.com>
2026-02-10 11:47:00 +0530

    added dockerfile

--- pg_4.py
+++ pg_4.py
@@ -1,9 +1,14 @@
 import os
 folder=input("please provide list of foler name: ").split()
 for folder in folder:
-    
-    files=os.listdir(folder)
-    print("==== lisiting fies for the folder -"+folder)
+    try:
+
+     files=os.listdir(folder)
+    except FileNotFoundError: 
+     print("==== lisiting fies for the folder -"+folder)
+    except PermissionError:
+       break
+
 
     for file in files:
         print(file)
